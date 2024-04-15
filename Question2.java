00e0fb7ba11cbdee50da5100940549a880cfacc6
NPTimothy <150310244+NPTimothy@users.noreply.github.com>
2024-04-15 14:34:19 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -10,7 +10,17 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
      
+    // Scanner in = new Scanner(System.in);
+    // double height = in.nextDouble();
+
     Scanner in = new Scanner(System.in);
-    double height = in.nextDouble();
+    System.out.print("Height: ");
+    Double height = in.nextDouble();
+      
+    System.out.print("Weight: ");
+    Double weight = in.nextDouble();
+
+    System.out.println("BMI: " + weight / (height * height));
+    in.close();
   }
 }
